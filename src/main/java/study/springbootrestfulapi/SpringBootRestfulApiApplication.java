c97e195d6985dc5560bfce419a5e8c4118fe56e4
ansehyeong <dkstpgud@gmail.com>
2022-01-26 19:41:32 +0900

    init spring boot restful api study

---
+++ src/main/java/study/springbootrestfulapi/SpringBootRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package study.springbootrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestfulApiApplication.class, args);
+    }
+
+}
